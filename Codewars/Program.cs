021b0fc9fbe8692ef16993150c83f699f9f63717
Urkellitow <Shkelqimcakaj@hotmail.com>
2021-09-29 13:56:48 +0200

    //How old will I be in 2099?

--- Codewars/Program.cs
+++ Codewars/Program.cs
@@ -12,17 +12,70 @@ namespace Codewars
     {
         static void Main(string[] args)
         {
-            int[] a = new int[] { 121, 144, 19, 161, 19, 144, 19, 11 };
-            int[] b = new int[] { 11 * 11, 121 * 121, 144 * 144, 19 * 19, 161 * 161, 19 * 19, 144 * 144, 19 * 19 };
+            Console.WriteLine(CalculateAge(1993,2021));
+        }
+        //How old will I be in 2099? - 8kyu
+        public static string CalculateAge(int birth, int yearTo)
+        {
+            if (birth == yearTo)
+                return "You were born this very year!";
+            if (birth + 1 == yearTo)
+                return $"You are {yearTo - birth} year old.";
+            if (birth == yearTo + 1)
+                return $"You will be born in { birth - yearTo} year.";
+            if (birth > yearTo)
+               return $"You will be born in { birth - yearTo} years.";
+            if (birth < yearTo)
+                return $"You are {yearTo - birth} years old.";
 
-
-
-            var result = comp(a, b);
-
-
+            return "";
         }
 
+        //Row of the odd triangle - 6kyu /todo
+        public static long[] OddRow(int n)        
+        {
+            long[] result = new long[n];
+            ulong first = (ulong)(n * (n - 1)+1);
+            for (int j = 0; j < n; j++)
+            {
+                result[j] = (long)first;
+                first += 2;
+            }
 
+            return result;
+        }
+
+        //Encrypt this! - 6kyu - lyckades inte riktigt lösa denna..  //todo
+        public static string EncryptThis(string input)
+        {
+
+            var test = input.Split(' ').ToList();
+
+            for (int i = 0; i < test.Count; i++)
+            {
+                if (test[i].Length == 1)
+                {
+                    int t = test[i][0];
+                    test[i] = test[i].Replace(test[i], t.ToString());
+                    continue;
+                }
+                if (test[i].Length > 1)
+                {
+                    int t = test[i][0];
+                    char temp = test[i][1];
+                    test[i] = test[i].Replace(test[i][1], test[i][^1]);
+                    test[i] = test[i].Remove(test[i].Length - 1, 1);
+                    // test[i] = test[i].Replace(test[i][1], temp);
+                    test[i] = test[i].Insert(test[i].Length, temp.ToString());
+                    test[i] = test[i].Remove(0, 1);
+                    test[i] = test[i].Insert(0, t.ToString());
+
+                }
+
+            }
+
+            return string.Join(' ', test);
+        }
         //Are they the "same"? - 6kyu
         public static bool comp(int[] a, int[] b)
         {
@@ -34,7 +87,10 @@ namespace Codewars
             a = a.Select(x => x * x).ToArray();
 
             return a.SequenceEqual(b);
-
+            /*
+                if (a == null || b == null) return false;
+    var result = a.Select(x => x*x).OrderBy(x=>x).SequenceEqual(b.OrderBy(x=>x));
+    return result;*/
         }
         //Array.diff - 6kyu
         public static int[] ArrayDiff(int[] a, int[] b)
@@ -56,7 +112,6 @@ namespace Codewars
 
             return result.ToArray();
         }
-
         //Which string is worth more? - 7kyu
         public static string HighestValue(string a, string b)
         {
@@ -138,35 +193,6 @@ namespace Codewars
         public static int StrCount(string str, string letter)
         {
             return str.Count(x => x.ToString() == letter);
-        }
-        //Encrypt this! - 6kyu - lyckades inte riktigt lösa denna..  //todo
-        public static string EncryptThis(string input)
-        {
-
-                var test = input.Split(' ').ToList();
-
-                for (int i = 0; i < test.Count; i++)
-                {
-                    if (test[i].Length == 1)
-                    {
-                        int t = test[i][0];
-                        test[i] = test[i].Replace(test[i], t.ToString());
-                        continue;
-                    }
-                    if (test[i].Length > 1)
-                    {
-                        int t = test[i][0];
-                        char temp = test[i][1];
-                        test[i] = test[i].Replace(test[i][1], test[i][^1]);
-                        test[i] = test[i].Remove(test[i].Length-1, 1);
-                        // test[i] = test[i].Replace(test[i][1], temp);
-                        test[i] = test[i].Insert(test[i].Length, temp.ToString());
-                        test[i] = test[i].Replace(test[i][0].ToString(), t.ToString());
-                    }
-
-                }
-
-                return string.Join(' ', test);
         }
         //Last Survivor
         public static string LastSurvivor(string letters, int[] coords)
